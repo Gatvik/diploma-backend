6c7168532f457e7a1f2e94299205a4430c20f682
Kirill <71784869+Gatvik@users.noreply.github.com>
2024-11-27 11:25:01 +0100

    Added GetAllAssignmentsToUserQuery

---
+++ Api/Application/Features/AssignmentToUser/Queries/GetAll/GetAllAssignmentsToUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using Api.Application.Features.AssignmentToUser.Common;
+using MediatR;
+
+namespace Api.Application.Features.AssignmentToUser.Queries.GetAll;
+
+public class GetAllAssignmentsToUserQuery : IRequest<List<AssignmentToUserDto>>
+{
+    
+}
---
+++ Api/Application/Features/AssignmentToUser/Queries/GetAll/GetAllAssignmentsToUserQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using Api.Application.Contracts.Persistence;
+using Api.Application.Exceptions;
+using Api.Application.Features.AssignmentToUser.Common;
+using AutoMapper;
+using MediatR;
+
+namespace Api.Application.Features.AssignmentToUser.Queries.GetAll;
+
+public class GetAllAssignmentsToUserQueryHandler : IRequestHandler<GetAllAssignmentsToUserQuery, List<AssignmentToUserDto>>
+{
+    private readonly IAssignmentToUserRepository _assignmentToUserRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllAssignmentsToUserQueryHandler(IAssignmentToUserRepository assignmentToUserRepository, IMapper mapper)
+    {
+        _assignmentToUserRepository = assignmentToUserRepository;
+        _mapper = mapper;
+    }
+    
+    public async Task<List<AssignmentToUserDto>> Handle(GetAllAssignmentsToUserQuery request, CancellationToken cancellationToken)
+    {
+        var assignments = await _assignmentToUserRepository.GetAllAsync(incl => incl.Assignment.Role, incl => incl.User);
+        if (assignments.Count == 0)
+            throw new NotFoundException();
+
+        return _mapper.Map<List<AssignmentToUserDto>>(assignments);
+    }
+}
