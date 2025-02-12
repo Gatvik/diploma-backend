2ad54e2925810aa9cc1f1e5508df381a1a48b37e
Kirill <71784869+Gatvik@users.noreply.github.com>
2025-02-12 15:13:53 +0100

    Added filtration by date to AssignmentsToUsersController.cs GET ALL OWN

---
+++ Api/Application/Features/AssignmentToUser/Queries/GetAllOwnByDate/GetAllOwnAssignmentsByDateQuery.cs
@@ -0,0 +1,10 @@
+﻿using Api.Application.Features.AssignmentToUser.Common;
+using MediatR;
+
+namespace Api.Application.Features.AssignmentToUser.Queries.GetAllOwnByDate;
+
+public class GetAllOwnAssignmentsByDateQuery : IRequest<List<AssignmentToUserDto>>
+{
+    public int Month { get; set; }
+    public int Year { get; set; }
+}
---
+++ Api/Application/Features/AssignmentToUser/Queries/GetAllOwnByDate/GetAllOwnAssignmentsByDateQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using Api.Application.Contracts.Identity;
+using Api.Application.Contracts.Persistence;
+using Api.Application.Exceptions;
+using Api.Application.Features.AssignmentToUser.Common;
+using AutoMapper;
+using MediatR;
+
+namespace Api.Application.Features.AssignmentToUser.Queries.GetAllOwnByDate;
+
+public class GetAllOwnAssignmentsByDateQueryHandler : IRequestHandler<GetAllOwnAssignmentsByDateQuery, List<AssignmentToUserDto>>
+{
+    private readonly IAssignmentToUserRepository _assignmentToUserRepository;
+    private readonly IMapper _mapper;
+    private readonly IUserService _userService;
+
+    public GetAllOwnAssignmentsByDateQueryHandler(IAssignmentToUserRepository assignmentToUserRepository, IMapper mapper,
+        IUserService userService)
+    {
+        _assignmentToUserRepository = assignmentToUserRepository;
+        _mapper = mapper;
+        _userService = userService;
+    }
+    
+    public async Task<List<AssignmentToUserDto>> Handle(GetAllOwnAssignmentsByDateQuery request, CancellationToken cancellationToken)
+    {
+        var assignments = await _assignmentToUserRepository
+            .GetAllByPredicateAsync(predicate: p => 
+                    p.User.Id == Guid.Parse(_userService.UserId) 
+                    && p.StartTime.Month == request.Month 
+                    && p.StartTime.Year == request.Year,
+                includes: incl => incl.Assignment.Role);
+
+        if (assignments.Count == 0)
+            throw new NotFoundException("No assignments");
+        
+        return _mapper.Map<List<AssignmentToUserDto>>(assignments);
+    }
+}
